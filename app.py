1c9b3a8c46330d2f57f0641d0b936d3ed5b03dea
Mohd Noor Syawal Faqqie Bin Abdullah <102709074+SyawalFaqqie@users.noreply.github.com>
2023-02-27 17:55:32 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.title("My :green[Data App] :rics_cracker:")
+st.header("Data Science Internship")
+
+st.snow()
+
+btn_click = st.button("Click Me!")
+
+if btn_click == True:
+    st.subheader("You clicked me :sunglasses:")
+    st.balloons()
